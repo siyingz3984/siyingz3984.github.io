4104bb4963459cd4414198369e7bda74844a3e6f
siyingz3984 <91745012+siyingz3984@users.noreply.github.com>
2021-10-01 11:16:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "SiSi"
+let userName = "siyingz3984";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
